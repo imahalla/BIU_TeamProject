8a7bdc060e1637b8ac087d616dfe4d1301d3096e
israel mahalla <israel.mahalla@gmail.com>
2023-09-07 00:07:24 +0300

    terrafom

--- terraform/network.tf
+++ terraform/network.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_subnet" "priavate_subnet" {
+resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   tags = {
